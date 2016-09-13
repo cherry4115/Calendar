aa718d275456b8296040cbe35d281654e49d766b
cherry4115 <1541683819@qq.com>
2016-09-13 10:13:40 +0800

    raf兼容性扩展

--- javascripts/calendar.js
+++ javascripts/calendar.js
@@ -1184,6 +1184,37 @@ function documentEvent(){
     }
 }
 
+/**
+ * requestAnimationFrame兼容性扩展，两方面工作：
+ * 1、把各浏览器前缀进行统一
+ * 2、在浏览器没有requestAnimationFrame方法时将其指向setTimeout方法
+ * */
+function RAF_compat() {
+    var lastTime = 0;
+    var vendors = ["webkit", "moz"];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
+        // Webkit中此取消方法的名字变了
+        window.cancelAnimationFrame = window[vendors[x] + "CancelAnimationFrame"] || window[vendors[x] + "CancelRequestAnimationFrame"];
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+            var id = window.setTimeout(function() {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+    }
+}
+
 
 /*
  * 花瓣的运动轨迹
@@ -1248,6 +1279,9 @@ window.onload=function(){
 
     //调用事件函数
     documentEvent();
+
+    //requestAnimationFrame兼容性扩展
+    RAF_compat();
 
     //飘落花瓣
     showPetals();
